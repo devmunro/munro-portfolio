4ccb13a3dcf377ac1c799ae3bbdd8dadd1991645
devmunro <defmunro@gmail.com>
2024-06-22 09:56:33 +0100

    intital commit

---
+++ app/layout/Navbar.tsx
@@ -0,0 +1,16 @@
+
+const Navbar: React.FC = () => {
+  return (
+    <nav >
+      <ul className="flex items-end justify-around">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
