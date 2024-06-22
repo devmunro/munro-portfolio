2083aff7daf5f5877e9f7f31624da092e94f8862
devmunro <defmunro@gmail.com>
2024-06-22 10:25:19 +0100

    chore: Update Navbar and page layout

--- app/layout/Navbar.tsx
+++ app/layout/Navbar.tsx
@@ -1,13 +1,11 @@
-
 const Navbar: React.FC = () => {
   return (
-    <nav >
-      <ul className="flex items-end justify-around">
+    <nav className="fixed top-0 left-0 w-full p-4 bg-black z-10">
+      <ul className="flex justify-end space-x-4">
         <li>Home</li>
         <li>About</li>
         <li>Projects</li>
         <li>Contact</li>
-
       </ul>
     </nav>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Navbar from "./layout/Navbar";
+import Header from "./layout/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-black text-white ">
       <Navbar />
+      <div className="pt-16 h-screen">
+        <Header />
+      </div>
     </div>
   );
 }
