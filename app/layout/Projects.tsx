3fdbef9d5ad56ef82435e07ce86513c4755b3b30
devmunro <defmunro@gmail.com>
2024-06-23 09:27:40 +0100

    refactor: Improve Projects component layout and filtering
    Refactor the Projects component layout to include a shadow effect, adjust the carousel content positioning, and add filtering functionality based on selected skills. This improves the visual appearance, consistency, and user experience of the Projects section.
    
    #chore

--- app/layout/Projects.tsx
+++ app/layout/Projects.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Carousel,
   CarouselContent,
@@ -7,34 +8,113 @@ import {
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
+import { useState } from "react";
 
 const projects = () => {
-  const projects = [
+  const projectShowcase = [
     {
-      title: "Project One",
+      title: "JS Project",
       image: "https://via.placeholder.com/250",
+      tools: ["javascript", "mongodb", "tailwind css"],
     },
     {
-      title: "Project Two",
+      title: "Python Project",
       image: "https://via.placeholder.com/150",
+      tools: ["python", "django", "sqlite"],
     },
     {
-      title: "Project Three",
+      title: "Next.JS Project",
       image: "https://via.placeholder.com/150",
+      tools: ["next.js", "react", "node.js"],
     },
     {
-      title: "Project Four",
+      title: "PHP Project",
       image: "https://via.placeholder.com/150",
+      tools: ["php", "laravel", "mysql"],
     },
     {
-      title: "Project Five",
+      title: "JS Project 2",
       image: "https://via.placeholder.com/150",
+      tools: ["javascript", "express", "bootstrap"],
     },
   ];
+  const [filteredProjects, setFilteredProjects] = useState(projectShowcase);
+
+  const languageOptions = [
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "PHP",
+    "Java",
+    "C#",
+  ];
+  const frameworkOptions = [
+    "React",
+    "Angular",
+    "Vue.js",
+    "Next.js",
+    "Express.js",
+    "Laravel",
+  ];
+  const databaseOptions = ["MongoDB", "MySQL", "PostgreSQL", "Firebase"];
+  const frontEndLibraries = ["Tailwind CSS", "Bootstrap"];
+
+  const handleFilter = (value) => {
+    console.log(value)
+    const includedProjects = projectShowcase.filter((project) =>
+      project.tools.includes(value.toLowerCase())
+    );
+    setFilteredProjects(includedProjects)
+  };
 
   return (
     <div className="flex bg-gray-400 text-black h-full items-center p-20">
-      <div className="w-1/2">BADGES</div>
+      <div className="w-1/2">
+        <h1>SKILLS</h1>
+        <div>
+          <label className=" mb-2 font-bold">Languages:</label>
+          <ul className="flex flex-wrap space-x-2">
+            {languageOptions.map((option, index) => (
+              <li key={index} value={option}>
+                <Button onClick={() => handleFilter(option)}>{option}</Button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <label className=" mb-2 font-bold">Frameworks:</label>
+          <ul className="flex  flex-wrap space-x-2 ">
+            {frameworkOptions.map((option, index) => (
+              <li key={index} value={option}>
+                <Button onClick={() => handleFilter(option)}>{option}</Button>
+                </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <label className=" mb-2 font-bold">Database:</label>
+          <ul className="flex flex-wrap space-x-2 ">
+            {databaseOptions.map((option, index) => (
+              <li key={index} value={option}>
+                <Button onClick={() => handleFilter(option)}>{option}</Button>
+                </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <label className=" mb-2 font-bold">CSS Libraries:</label>
+          <ul className="flex flex-wrap space-x-2 ">
+            {frontEndLibraries.map((option, index) => (
+              <li key={index} value={option}>
+                <Button onClick={() => handleFilter(option)}>{option}</Button>
+                </li>
+            ))}
+          </ul>
+        </div>
+      </div>
       <div className=" w-1/2">
         <Carousel
           opts={{
@@ -44,13 +124,13 @@ const projects = () => {
           className=" shadow-xl"
         >
           <CarouselContent className="-mt-1 h-[700px]">
-            {projects.map((project, index) => (
+            {filteredProjects.map((project, index) => (
               <CarouselItem
                 key={index}
                 className="pt-1 w-full h-full basis-1/2"
               >
                 <Card className="h-full relative">
-                  <CardContent className="flex items-center justify-center p-6 h-full">
+                  <CardContent className="flex items-center justify-center p-6 h-full relative">
                     <Image
                       src={project.image}
                       alt={project.title}
@@ -59,6 +139,16 @@ const projects = () => {
                       quality={100}
                       priority
                     />
+                    <h3 className="bottom-0 left-0 absolute">
+                      {project.title}
+                    </h3>
+                    <ul className="bottom-0 right-0 absolute flex">
+                      {project.tools.map((tool, index) => (
+                        <li key={index}>
+                          <Badge className="">{tool}</Badge>
+                        </li>
+                      ))}
+                    </ul>
                   </CardContent>
                 </Card>
               </CarouselItem>
